8f8c278e47a0ebea1691180c20ee258046da5a8b
gitloyunemo <3210100968@zju.edu.cn>
2024-05-07 21:37:57 +0800

    add original instructions

--- Hello.py
+++ Hello.py
@@ -30,19 +30,7 @@ def run():
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        This is a Website for cloud prediction. you can query for the Ground-based sky imagery photographed by a fish-eye camera now and get the preidction by our preediction model.
     """
     )
 
